14a94e35e0190160fea49cc876354f8a8a90e3ea
shivaprasadp <shivaprasad.ph.ext@lastminute.com>
2025-07-13 23:33:10 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>hello shiva</h1>
+
+    </>
+  )
+}
+
+export default App
